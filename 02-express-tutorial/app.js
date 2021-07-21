085390f7fc82171fb0093dc922a3dd45d9a93e67
DaviD D <jdavid.martinez@gmail.com>
2021-07-21 09:47:42 -0500

    Json Example

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,21 +1,14 @@
 const express = require('express');
-const app = express()
+
+const app = express();
+
+const { products }= require('./data')
 
 app.get('/', (req, res) => {
-    console.log('user hit the resource')
-res.status(200).send('Home page')
+    res.json(products)
 })
 
-app.get('/about',(req, res) => {
-    console.log('user hit the resource /about')
-res.status(200).send('About page')
-})
 
-app.all('*',(req, res) => {
-    console.log('user hit the resource *')
-res.status(404).send('<h1> Resource not found </h1>')
-})
-
-app.listen(5000, () => {
-    console.log('server is listening on port 5000')
+app.listen(5000, () =>{
+    console.log('Server is listening on port 5000')
 })
