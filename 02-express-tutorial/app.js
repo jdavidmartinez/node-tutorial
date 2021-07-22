331babedb0bc5ec79c5ae77d28ef02174cb55b56
DaviD D <jdavid.martinez@gmail.com>
2021-07-22 16:36:26 -0500

    Method GET

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,26 +1,11 @@
 const express = require("express");
-const logger = require('./logger')
-const authorize = require('./authorize')
-
-
 const app = express();
 
-app.use([logger, authorize])
+let { people } = require('./data')
 
-app.get("/",  (req, res) => {
-  res.send("Home");
-});
-
-app.get("/about",  (req, res) => {
-  res.send("About");
-});
-app.get("/api/products", (req, res) => {
-  res.send("Products");
-});
-app.get("/api/items",  (req, res) => {
-  console.log(req.users )
-  res.send("Items");
-});
+app.get('/api/people', (req,res) => {
+  res.status(200).send({success: true, data: people})
+})
 
 app.listen(5000, () => {
   console.log("Server is listening on port 5000");
